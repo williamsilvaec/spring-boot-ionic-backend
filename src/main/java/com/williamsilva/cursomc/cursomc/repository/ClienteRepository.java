6af3aad38cf385468c568c9ca09cd0c886f3f41a
william.silva <william@viasoft.com.br>
2018-03-15 10:25:38 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/williamsilva/cursomc/cursomc/repository/ClienteRepository.java
+++ src/main/java/com/williamsilva/cursomc/cursomc/repository/ClienteRepository.java
@@ -2,6 +2,12 @@ package com.williamsilva.cursomc.cursomc.repository;
 
 import com.williamsilva.cursomc.cursomc.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Optional<Cliente> findByEmail(String email);
 }
--- src/main/java/com/williamsilva/cursomc/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/williamsilva/cursomc/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,16 +1,23 @@
 package com.williamsilva.cursomc.cursomc.services.validation;
 
 import com.williamsilva.cursomc.cursomc.dto.ClienteNewDTO;
+import com.williamsilva.cursomc.cursomc.model.Cliente;
 import com.williamsilva.cursomc.cursomc.model.enums.TipoCliente;
+import com.williamsilva.cursomc.cursomc.repository.ClienteRepository;
 import com.williamsilva.cursomc.cursomc.resources.handler.FieldMessage;
 import com.williamsilva.cursomc.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clientes;
 
     @Override
     public void initialize(ClienteInsert clienteInsert) {
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Optional<Cliente> cliente = clientes.findByEmail(clienteNewDTO.getEmail());
+
+        if (cliente.isPresent()) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
